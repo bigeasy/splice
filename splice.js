f341ca0cdc9c87ae4bd8f11d2d2e9f80f8d41edd
Alan Gutierrez <alan@prettyrobots.com>
2015-02-04 08:25:24 -0600

    Tidy for conversion to Cadence Redux.
    See #48.

--- splice.js
+++ splice.js
@@ -8,58 +8,63 @@ function Splice (operation, primary, iterator) {
 }
 
 Splice.prototype.splice = cadence(function (async) {
-    async(function () {
+    var iterator = async(function () {
         this._iterator.next(async())
     }, function (record, key) {
-        var operate
-        if (record && key) operate = async(function () {
-           if (!this._mutator) {
-                this._primary.mutator(key, async(async)(function ($) {
-                    this._mutator = $
-                    return this._mutator.index
-                }))
-            } else {
-                this._mutator.indexOf(key, async())
-            }
-        }, function (index) {
-            if (index < 0) {
-                if (this._mutator.length < ~index) {
+        if (record && key) {
+            var operate = async(function () {
+                if (!this._mutator) {
                     async(function () {
-                        this._mutator.unlock(async())
-                    }, function () {
-                        delete this._mutator
-                        return [ operate() ]
+                        this._primary.mutator(key, async())
+                    }, function (mutator) {
+                        return (this._mutator = mutator).index
                     })
                 } else {
-                    return [ ~index, null ]
-                }
-            } else {
-                var item = this._mutator.get(index)
-                return [ index, { record: item.record, key: item.key } ]
-            }
-        }, function (index, existing) {
-            var operation = this._operation({ record: record, key: key }, existing)
-            async(function () {
-                if ((operation == 'insert' || operation == 'delete') && existing) {
-                    async(function () {
-                        this._mutator.remove(index, async())
-                    }, function () {
-                        this._mutator.indexOf(key, async())
-                    }, function (index) {
-                        return [ ~index ]
-                    })
-                } else {
-                    return [ index ]
+                    this._mutator.indexOf(key, async())
                 }
             }, function (index) {
-                if (operation == 'insert') {
-                    async(function () {
-                        this._mutator.insert(record, key, index, async())
-                    })
+                if (index < 0) {
+                    if (this._mutator.length < ~index) {
+                        async(function () {
+                            this._mutator.unlock(async())
+                        }, function () {
+                            delete this._mutator
+                            return [ operate() ]
+                        })
+                    } else {
+                        return [ ~index, null ]
+                    }
+                } else {
+                    var item = this._mutator.get(index)
+                    return [ index, { record: item.record, key: item.key } ]
                 }
+            }, function (index, existing) {
+                var operation = this._operation({ record: record, key: key }, existing)
+                async(function () {
+                    if ((operation == 'insert' || operation == 'delete') && existing) {
+                        async(function () {
+                            this._mutator.remove(index, async())
+                        }, function () {
+                            this._mutator.indexOf(key, async())
+                        }, function (index) {
+                            return [ ~index ]
+                        })
+                    } else {
+                        return [ index ]
+                    }
+                }, function (index) {
+                    if (operation == 'insert') {
+                        async(function () {
+                            this._mutator.insert(record, key, index, async())
+                        })
+                    }
+                })
+            }, function () {
+                return [ operate ]
             })
-        })(1)
-        else return [ async ]
+        } else {
+            return [ iterator ]
+        }
     })()
 })
 
