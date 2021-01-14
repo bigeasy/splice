8c464948bf54ec6551fb3cf64fcbc489cd58dd7d
Alan Gutierrez <alan@prettyrobots.com>
2021-01-13 23:05:54 -0600

    Upgrade `b-tree` to `2.0.0-alpha.83`.
    https://github.com/bigeasy/strata/releases/tag/v2.0.0-alpha.83

--- test/splice.t.js
+++ test/splice.t.js
@@ -32,7 +32,7 @@ require('proof')(1, async okay => {
         const turnstile = new Turnstile(destructible.durable($ => $(), 'turnstile'))
         const pages = new Magazine
         const handles = new Operation.Cache(new Magazine)
-        const storage = await FileSystem.open({ directory, handles })
+        const storage = new FileSystem.Writer(destructible.durable($ => $(), 'storage'), await FileSystem.open({ directory, handles }))
         destructible.rescue($ => $(), 'test', async () => {
             const strata = new Strata(destructible.durable($ => $(), 'strata'), { storage, pages, turnstile })
             const twiddled = twiddle(advance([
